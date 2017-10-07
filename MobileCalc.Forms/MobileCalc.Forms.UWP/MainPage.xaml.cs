8c158902eddf172e9cfbb22f952cc509cb12d2a2
Lee Richardson <github@leerichardson.com>
2017-10-06 21:29:34 -0400

    Add project files.

---
+++ MobileCalc.Forms/MobileCalc.Forms.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MobileCalc.Forms.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new MobileCalc.Forms.App());
+        }
+    }
+}
---
+++ MobileCalc.Forms/MobileCalc.Forms/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using MobileCalc.Forms.ViewModels;
+
+using Xamarin.Forms;
+
+namespace MobileCalc.Forms.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
